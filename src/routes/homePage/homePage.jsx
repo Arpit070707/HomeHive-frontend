38ad24c8a5c839496bf69e90b98b68b8097da218
Arpit Gutpa <arpitgupta050505@gmail.com>
2024-11-03 04:03:31 +0530

    HomePage Start

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,12 @@
+import './homePage.scss'
+
+function HomePage(){
+    return(
+        <div className='homePage'>
+            <div className="textContainer"></div>
+            <div className="imgContainer"></div>
+        </div>
+    )
+}
+
+export default HomePage
